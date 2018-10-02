e07ad1aa9a25af23928db20269598904b4b25d37
Preston Smith (work) <PrestonII@users.noreply.github.com>
2018-10-02 15:24:36 -0400

    start of repo

---
+++ AirChangeTracer/SpacePropertyService.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB.Mechanical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirChangeTracer
+{
+    public class SpacePropertyService
+    {
+        public void GetSpaceTypeAsString(Space sp)
+        {
+            var sp_type = sp.SpaceType.ToString();
+            
+        }
+
+    }
+}
---
+++ AirChangeTracer/VentilationResolver.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirChangeTracer
+{
+    public class VentilationResolver : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        // sketch of intended work
+        public void Nothing()
+        {
+            /*
+             *  check the lookup table
+             * 
+             *  compare the space type
+             * 
+             */
+
+            // add/find parameter called AIR_CHANGE_REQS
+            // assign based on lookup table
+            // 
+        }
+    }
+}
