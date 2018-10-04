72b4aac3b767bc713a909d87fd67c7adaffa5594
Preston Smith (work) <PrestonII@users.noreply.github.com>
2018-10-04 15:36:35 -0400

    calculating space properties

---
+++ HIVE.Revit/Services/ElementLocationService.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Revit.Services
+{
+    public class ElementLocationService
+    {
+        public static IEnumerable<Element> FindElementsInSameSpace(Element element)
+        {
+            var doc = element.Document;
+            var collected = new FilteredElementCollector(doc)
+                .OfCategoryId(element.Id)
+                .WherePasses(new ElementIntersectsElementFilter(element))
+                .ToElements();
+
+            return collected;
+        }
+
+        public static IEnumerable<T> FindElementsInSameSpace<T>(Element element) where T : Element
+        {
+            var elem = FindElementsInSameSpace(element).Cast<T>();
+
+            return elem;
+        }
+
+        public static T FindElementInSameSpace<T>(Element element) where T: Element
+        {
+            return FindElementsInSameSpace<T>(element).First();
+        }
+
+        public static Element FindElementInSameSpace(Element element)
+        {
+            return FindElementsInSameSpace(element).First();
+        }
+    }
+}
--- HIVE.Revit/Services/SpacePropertyService.cs
+++ HIVE.Revit/Services/SpacePropertyService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB;
+using Hive.Revit.Services;
 
 namespace AirChangeTracer
 {
@@ -19,12 +20,37 @@ namespace AirChangeTracer
 
         public static double CalculateCeilingHeight(Space space)
         {
+            return space?.Volume <= 0 
+                ? CalculateCeilingHeightByCeiling(space) 
+                : CalculateCeilingHeightByVolume(space);
+        }
+
+        public static double CalculateCeilingHeightByCeiling(Space space)
+        {
+            var ceiling = FindCeilingInSpace(space);
+            var clgLocation = (LocationPoint) ceiling.Location;
+            var level = (Level) space.Document.GetElement(ceiling.LevelId);
+            var levelHeight = level.Elevation;
+            var height = clgLocation.Point.Z - levelHeight;
+
+            return height;
+        }
+
+        public static double CalculateCeilingHeightByVolume(Space space)
+        {
             if(space.Volume == null)
                 throw new Exception();
 
             var height =  space.Volume / space.Area;
 
             return height;
+        }
+
+        public static Ceiling FindCeilingInSpace(Space space)
+        {
+            var ceiling = ElementLocationService.FindElementInSameSpace<Ceiling>(space);
+
+            return ceiling;
         }
 
         public static double GetOutsideAirFromSpace(Space space)
