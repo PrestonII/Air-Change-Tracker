50f2f8e1082a58d76266d20abc8285dff391549b
Preston Smith (work) <PrestonII@users.noreply.github.com>
2018-10-12 10:55:48 -0400

    can avoid readding parameters to schedule more easily

--- HIVE.Revit/Services/RevitParameterUtility.cs
+++ HIVE.Revit/Services/RevitParameterUtility.cs
@@ -164,13 +164,13 @@ namespace Hive.Revit.Services
 
         public static bool ScheduleHasParameter(ViewSchedule schedule, Parameter parameter)
         {
-            var data = schedule.GetTableData().GetSectionData(SectionType.Body);
-            var count = data.NumberOfColumns;
+            var bodyData = schedule.GetTableData().GetSectionData(SectionType.Body);
+            var count = bodyData.NumberOfColumns;
 
             for (var i = 0; i < count; i++)
             {
-                var id = data.GetCellParamId(i); // figure out things here
-                var name = data.GetCellText(0, i);
+                var field = schedule.Definition.GetField(i);
+                var id = field.ParameterId;
 
                 if (id == parameter.Id)
                     return true;
